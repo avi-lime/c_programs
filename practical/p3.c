8195bdf04da00ca44c5734b19708894c80bbdf89
Avi <sa3198154@gmail.com>
2023-10-28 18:45:54 +0530

    Update p3.c

--- practical/p3.c
+++ practical/p3.c
@@ -9,17 +9,16 @@ Date: 13/10/2023
 
 int main()
 {
-    int principal, rate, time;
-    float SI;
-    double CI;
+    double principal, rate, time, SI, Amount, CI;
     printf("\nEnter Principal Amount: ");
-    scanf("%d", &principal);
+    scanf("%lf", &principal);
     printf("\nEnter Rate of interest: ");
-    scanf("%d", &rate);
+    scanf("%lf", &rate);
     printf("\nEnter Time: ");
-    scanf("%d", &time);
+    scanf("%lf", &time);
     SI = (principal * rate * time)/100 ; //Simple Interest formula
-    CI = (principal*(pow((1 + rate / 100), time)) - principal); //Compound Interest formula
-    printf("\nThe Simple Interest is %.2f\nThe Compound Interest is %.2lf", SI, CI);
+    Amount = principal*(pow((1 + rate / 100), time)); //Compound Interest formula
+    CI = Amount - principal;
+    printf("\nThe Simple Interest is %.2lf\nThe Compound Interest is %.2lf", SI, CI);
     return 0;
 }
